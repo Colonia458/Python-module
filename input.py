733c4dd098cf011d2668ca52e7557c8d61479ba2
Griffins Gitahi <116430180+Colonia458@users.noreply.github.com>
2025-08-12 16:20:41 +0300

    Update input.py

--- input.py
+++ input.py
@@ -1,22 +1,22 @@
 file = open("input.txt", "r")
-# print(file.read())
+print(file.read())
 
-# print(file.readline())
-# print(file.readline())
+print(file.readline())
+print(file.readline())
 
-# for line in file:
-#     print(line)
-#     file.close()
+for line in file:
+     print(line)
+     file.close()
 
 
-# try:
-#     file = open("Jimbo.txt")
-#     print(file.read())
-# except:
-#     print("That file doesn't exist")
-# finally:
-#     file.close()
-# file.close()
+ try:
+     file = open("Jimbo.txt")
+     print(file.read())
+ except:
+     print("That file doesn't exist")
+ finally:
+     file.close()
+ file.close()
 
 
 file = open("input.txt", "a")
